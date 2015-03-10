4e269e8cd78f157a069bcef0169134f5dc8f69b9
Venkata Aditya Korada <venkata@korada.in>
2015-03-10 00:30:59 -0400

    fix build

--- app/data/posts.js
+++ app/data/posts.js
@@ -3,22 +3,11 @@
 ---
 define([],function(){
 	return [
-
-<<<<<<< HEAD:app/data/posts.js
-=======
-functionCall({
-    "posts":[
->>>>>>> origin/master:data/site.json
     {% for post in site.posts %}
         {
           "title"    : "{{ post.title }}",
           "url"     : "{{ post.url }}",
           "date"     : "{{ post.date | date: "%B %d, %Y" }}"
         } {% if forloop.last %}{% else %},{% endif %}
-<<<<<<< HEAD:app/data/posts.js
     {% endfor %} ];
 });
-=======
-    {% endfor %}
-]}); 
->>>>>>> origin/master:data/site.json
