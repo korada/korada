fbd34e4fb00f4bc5053b0bed371b823d35def6db
Venkata Korada <venkata@korada.in>
2026-06-19 18:46:14 -0400

    gas update

--- client/components/BabyShower.jsx
+++ client/components/BabyShower.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import inviteImg from '../assets/images/seemantham-invite.jpg';
 import '../assets/css/BabyShower.scss';
 
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzPuwskCSm1QgWb3VvxXE5ktR75a3AArgbKDPLoh7OpB-BGr6MAUFnyM2wQnwojQS9t/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbw1nT7-VcUoYJhyalC_0lmkBABrSiPbqg0Mr23zq2JoVGLKPuFPKOp_NarH0B6bo0fl/exec';
 const STORAGE_KEY = 'seemantham_rsvp_v1';
 
 const PETALS = ['🌸', '🌼', '🌸', '🌷', '🌼', '🌸', '🌼', '🌷', '🌸', '🌼'];
